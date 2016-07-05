18c0464d579d1ab2ba1d2d000fbc3d5d2323ab89
TseHang <a54383813@gmail.com>
2016-07-05 18:10:15 +0800

    nav RWD

--- js/_nav.js
+++ js/_nav.js
@@ -2,17 +2,26 @@ var num = 9 ;
 
 $(document).scroll(function(){  
   $('.navbar').css({
-  	"backgroundColor":"rgba(2555,255,255,0.9)",
-  	"boxShadow":"0px 1px 10px -2px black"
+  	"backgroundColor":"rgba(2555,255,255,0.9)"
   })
 
   if ( $(window).scrollTop() < 80 ){
   	$('.navbar').css({
-  		"backgroundColor":"rgba(2555,255,255,0)",
-  		"boxShadow":"0px 0px 0px 0px black"
+  		"backgroundColor":"rgba(2555,255,255,0)"
   	});
   }
 });
+
+
+// //////////
+// 小icon
+// //////////
+$('.nav-small-icon').click(function(){
+	$('.navbar').toggleClass("nav-small-show");
+	$('.back-black').toggleClass("display-block");
+	$('.nav-small-bar').toggleClass("display-block");
+	$('.nav-small-icon').toggleClass("nav-small-icon-show");
+})
 
 // /////////////////
 // Mosquito-Router!!
@@ -43,6 +52,8 @@ $('#mosquito-qa').click(function(){
 $('#sub-logo').click(function(){
 	window.location.href = "../index2.html";
 })
+// 
+// ///////////////////////
 
 
 // 一直放大的實驗！！
