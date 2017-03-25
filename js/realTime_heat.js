2c0eaf56abb2c749d198a3e6b6cbcbe280746fc8
TseHang <a54383813@gmail.com>
2017-03-25 14:11:39 +0800

    fix realTime_heart token

--- js/realTime_heat.js
+++ js/realTime_heat.js
@@ -277,8 +277,8 @@ function heatMap(value) {
       $('.mapM').append('<div id="mapid"></div>');
 
       mymap = L.map('mapid').setView([22.9971, 120.2126], 15);
-
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+      
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
           '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
